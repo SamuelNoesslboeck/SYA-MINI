374ad8519299f050486f0ac01b5e58e643d5c36e
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2023-11-14 23:11:43 +0100

    additions

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,66 +1,13 @@
-use syact::{prelude::*, tool::Tongs};
+use syact::prelude::*;
 use sybot::prelude::*;
 
 mod sya_mini;
-use sya_mini::*;
 
 fn main() -> Result<(), sybot::Error> {
-    let sya_mini = SyaMiniRobot::new(
-        [ AngConf::default(); 4],
-        SyaMiniComps {
-            base: Gear::new(
-                Stepper::new(
-                    GenericPWM::new(
-                        0,
-                        0
-                    )?,
-                    StepperConst::MOT_17HE15_1504S
-                ),
-                1.0 / 9.0
-            ),
-            arm1: Gear::new(
-                Stepper::new(
-                    GenericPWM::new(
-                        0,
-                        0
-                    )?,
-                    StepperConst::MOT_17HE15_1504S
-                ),
-                1.0 / 9.0
-            ),
-            arm2: Gear::new(
-                Stepper::new(
-                    GenericPWM::new(
-                        0,
-                        0
-                    )?,
-                    StepperConst::MOT_17HE15_1504S
-                ),
-                1.0 / 9.0
-            ),
-            arm3: Gear::new(
-                Stepper::new(
-                    GenericPWM::new(
-                        0,
-                        0
-                    )?,
-                    StepperConst::MOT_17HE15_1504S
-                ),
-                1.0 / 9.0
-            )
-        },
-        vec![
-            Box::new(Tongs::new(
-                Servo::new(ServoConst::MG996R, 0),
-                0.4, 
-                0.8,
-                0.0,
-                Inertia(0.1)
-            ))
-        ]
-    );
+    let mut rob= sya_mini::sya_mini_rob()?;
+    let mut stat = sya_mini::SyaMiniStation::new();
 
-    
+    stat.home(&mut rob)?;
 
     Ok(())
 }
